39a5df42f99a31acfdc4da1219ba67698ec6e8eb
Bibin Baral <bipinbaral1225@gmail.com>
2026-08-13 21:40:50 +0545

    fix: platform-gate Windows workaround for Vercel Linux builds

--- patch-readlink.cjs
+++ patch-readlink.cjs
@@ -1,4 +1,11 @@
 const fs = require('fs');
+const path = require('path');
+
+// Strictly run ONLY on Windows to fix Node 22 libuv EISDIR & missing parent dir bugs.
+// Does NOTHING on Vercel/Linux so node fs is 100% standard on Linux deployments.
+if (process.platform !== 'win32') {
+  return;
+}
 
 function patchError(err) {
   if (err && (err.code === 'EISDIR' || err.errno === -4068 || err.errno === -4048)) {
@@ -9,12 +16,12 @@ function patchError(err) {
 
 if (fs.readlink) {
   const origReadlink = fs.readlink;
-  fs.readlink = function (path, options, callback) {
+  fs.readlink = function (filePath, options, callback) {
     if (typeof options === 'function') {
       callback = options;
       options = undefined;
     }
-    return origReadlink.call(this, path, options, (err, linkString) => {
+    return origReadlink.call(this, filePath, options, (err, linkString) => {
       if (callback) callback(patchError(err), linkString);
     });
   };
@@ -22,9 +29,9 @@ if (fs.readlink) {
 
 if (fs.readlinkSync) {
   const origReadlinkSync = fs.readlinkSync;
-  fs.readlinkSync = function (path, options) {
+  fs.readlinkSync = function (filePath, options) {
     try {
-      return origReadlinkSync.call(this, path, options);
+      return origReadlinkSync.call(this, filePath, options);
     } catch (err) {
       throw patchError(err);
     }
@@ -33,38 +40,74 @@ if (fs.readlinkSync) {
 
 if (fs.promises && fs.promises.readlink) {
   const origPromisesReadlink = fs.promises.readlink;
-  fs.promises.readlink = async function (path, options) {
+  fs.promises.readlink = async function (filePath, options) {
     try {
-      return await origPromisesReadlink.call(this, path, options);
+      return await origPromisesReadlink.call(this, filePath, options);
     } catch (err) {
       throw patchError(err);
     }
   };
 }
 
-if (fs.realpathSync) {
-  const origRealpathSync = fs.realpathSync;
-  fs.realpathSync = function (path, options) {
-    try {
-      return origRealpathSync.call(this, path, options);
-    } catch (err) {
-      if (err && (err.code === 'EISDIR' || err.errno === -4068 || err.errno === -4048)) {
-        return typeof path === 'string' ? path : path.toString();
+function ensureParentDir(destPath) {
+  try {
+    if (typeof destPath === 'string') {
+      const parent = path.dirname(destPath);
+      if (parent && !fs.existsSync(parent)) {
+        fs.mkdirSync(parent, { recursive: true });
       }
-      throw err;
     }
+  } catch (e) {}
+}
+
+if (fs.openSync) {
+  const origOpenSync = fs.openSync;
+  fs.openSync = function (filePath, flags, mode) {
+    if (typeof flags === 'string' && (flags.includes('w') || flags.includes('a') || flags.includes('+'))) {
+      ensureParentDir(filePath);
+    }
+    return origOpenSync.call(this, filePath, flags, mode);
   };
-  if (fs.realpathSync.native) {
-    const origNative = fs.realpathSync.native;
-    fs.realpathSync.native = function (path, options) {
-      try {
-        return origNative.call(this, path, options);
-      } catch (err) {
-        if (err && (err.code === 'EISDIR' || err.errno === -4068 || err.errno === -4048)) {
-          return typeof path === 'string' ? path : path.toString();
-        }
-        throw err;
-      }
-    };
-  }
+}
+
+if (fs.open) {
+  const origOpen = fs.open;
+  fs.open = function (filePath, flags, mode, callback) {
+    if (typeof mode === 'function') {
+      callback = mode;
+      mode = undefined;
+    }
+    if (typeof flags === 'function') {
+      callback = flags;
+      flags = 'r';
+    }
+    if (typeof flags === 'string' && (flags.includes('w') || flags.includes('a') || flags.includes('+'))) {
+      ensureParentDir(filePath);
+    }
+    return origOpen.call(this, filePath, flags, mode, callback);
+  };
+}
+
+if (fs.renameSync) {
+  const origRenameSync = fs.renameSync;
+  fs.renameSync = function (oldPath, newPath) {
+    ensureParentDir(newPath);
+    return origRenameSync.call(this, oldPath, newPath);
+  };
+}
+
+if (fs.rename) {
+  const origRename = fs.rename;
+  fs.rename = function (oldPath, newPath, callback) {
+    ensureParentDir(newPath);
+    return origRename.call(this, oldPath, newPath, callback);
+  };
+}
+
+if (fs.promises && fs.promises.rename) {
+  const origPromisesRename = fs.promises.rename;
+  fs.promises.rename = async function (oldPath, newPath) {
+    ensureParentDir(newPath);
+    return await origPromisesRename.call(this, oldPath, newPath);
+  };
 }
